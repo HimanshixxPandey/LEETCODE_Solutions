bf7799ba4ae12cf81bcbfd3417f6c9c9993f858b
HimanshixxPandey <100337639+HimanshixxPandey@users.noreply.github.com>
2022-11-22 02:25:40 +0530

    Time: 26 ms (49.47%), Space: 119.4 MB (61.55%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,43 +1,13 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        HashMap<Integer,ArrayList<Integer>>map=new HashMap<>();
-        List<Integer>list=new ArrayList<>();
-        for( List<Integer>a:edges)
-        {
-            List<Integer>l=a;
-            int k=a.get(0);
-            int v=a.get(1);
-            if(map.get(k)==null)
-            {
-                map.put(k, new ArrayList<Integer>());
-            }
-            map.get(k).add(v);
-        }
-        int arr[]=new int[n];
-       
-        System.out.println(map);
-        for(ArrayList<Integer> s:map.values())
-        {
-            ArrayList<Integer>ans=s;
-            for(int i=0;i<ans.size();i++)
-            {
-                arr[ans.get(i)]=1;
-                
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==0)
-            {
-                list.add(i);
-            }
-        }
-        if(list.size()==0)
-        {
-            list.add(edges.get(0).get(0));
-            return list;
-        }
-        return list;
-        
+         List<Integer> res = new ArrayList<>();
+        int[] seen = new int[n];
+        for (List<Integer> e: edges)
+            seen[e.get(1)] = 1;
+        for (int i = 0; i < n; ++i)
+            if (seen[i] == 0)
+                res.add(i);
+        return res;
+    
     }
 }
